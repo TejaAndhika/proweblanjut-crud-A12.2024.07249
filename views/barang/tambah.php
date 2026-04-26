b8125f78c501dac4caa68a25a388b5c4b46f8082
TejaAndhika <112202407249@mhs.dinus.ac.id>
2026-04-26 16:26:22 +0700

    Memperbaiki UI halaman tambah barang baru

--- views/barang/tambah.php
+++ views/barang/tambah.php
@@ -117,7 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Barang — Inventaris App</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../public/style.css">
     <style>
         .img-preview-wrap { margin-top: 10px; }
         .img-preview-wrap img {
